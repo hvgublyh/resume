a66597c84d9bc5845867c7fe692cb0a4c90186fe
hvgublyh <hvgublyh@outlook.com>
2025-03-18 19:03:20 +0800

    chore: 去掉components下build文件

--- packages/components/build/index.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-interface SiButtonProps {
-    children?: React.ReactNode;
-    onClick?: () => void;
-}
-declare const SiButton: React.FC<SiButtonProps>;
-
-export { SiButton as Button };
